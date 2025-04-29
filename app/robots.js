5794c0f2f6dcfb63a24e161aaf276fd72702c674
Gopal1853 <grmanian1853@gmail.com>
2025-04-29 17:45:00 +0530

    updated now

--- app/robots.js
+++ app/robots.js
@@ -1,17 +1,16 @@
-// export default function robots() {
-//     return {
-//         rules: [
-//             {
-//                 userAgent: 'Googlebot',
-//                 allow: ['/'],
-//                 disallow: ['/resume/'],
-//             },
-//             {
-//                 userAgent: ['Applebot', 'Bingbot'],
-//                 allow: ['/'],
-//                 disallow: ['/resume/'],
-//             },
-//         ],
-//         sitemap: 'https://theavnishkumar.in/sitemap.xml',
-//     }
-// }
+export default function robots() {
+    return {
+        rules: [
+            {
+                userAgent: 'Googlebot',
+                allow: ['/'],
+                disallow: ['/resume/'],
+            },
+            {
+                userAgent: ['Applebot', 'Bingbot'],
+                allow: ['/'],
+                disallow: ['/resume/'],
+            },
+        ],
+    }
+}
